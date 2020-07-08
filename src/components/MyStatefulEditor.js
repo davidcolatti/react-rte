92513783c2f243299a63ecf4ff873c60b66248c1
David Colatti <davidcolatti@gmail.com>
2020-07-08 16:43:31 -0400

    feat: regex to parse through the markdown

--- src/components/MyStatefulEditor.js
+++ src/components/MyStatefulEditor.js
@@ -5,11 +5,23 @@ const MyStatefulEditor = ({ onChange }) => {
   const [value, setValue] = useState(RichTextEditor.createEmptyValue());
 
   function handleChange(value) {
+    let markdown = value.toString("markdown");
+    handleRegex(markdown);
     setValue(value);
 
     if (onChange) {
-      onChange(value.toString("html"));
+      onChange(markdown);
     }
+  }
+
+  function handleRegex(markdown) {
+    // let chapterRegex = /^-(.*)/gm;
+    // let titleRegex = /( {4})-(.*)/gm;
+    // let chapterArray = markdown.match(chapterRegex);
+    // let titleArray = markdown.match(titleRegex);
+    let test = markdown.match(/^-(.*)|( {4})-(.*)/gm);
+
+    console.log(test);
   }
 
   return (
@@ -19,9 +31,10 @@ const MyStatefulEditor = ({ onChange }) => {
       <br />
       <br />
       <textarea
+        name="textarea"
         className="textarea"
         value={value.toString("markdown")}
-      ></textarea>
+      />
     </div>
   );
 };
