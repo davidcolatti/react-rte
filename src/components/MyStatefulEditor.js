88454bbe02fdb7fcaf1e1900774b0f08c78d0813
David Colatti <davidcolatti@gmail.com>
2020-07-09 17:33:57 -0400

    refactor: fixing regex

--- src/components/MyStatefulEditor.js
+++ src/components/MyStatefulEditor.js
@@ -15,33 +15,65 @@ const MyStatefulEditor = ({ onChange }) => {
   }
 
   function handleRegex(markdown) {
-    let test = markdown
-      .replace(/(^ {2}<li>)/gm, "parent - ")
+    debugger;
+    let dataArray = markdown
+      .replace(/(^ {2}<li>)/gm, "parent - ") // identifying what type of text it is
       .replace(/(^ {6}<li>)/gm, "direct child - ")
-      .replace(/(^ {10}<li>)/gm, "children - ");
+      .replace(/(^ {10}<li>)/gm, "children - ")
+      .split(">") // making all of the string an array
+      .filter(
+        // filtering array so we can work on only the text we need
+        (each) =>
+          each.includes("parent") ||
+          each.includes("direct child") ||
+          each.includes("children")
+      )
+      .map((each) => each.replace(/(\s)*<\/?\w{2}?/g, "").trim()); // mapping to remove unnecessary space and symbols
 
-    // let parentArray = test.match(/^parent -.*/gm);
-    // let directChildArray = test.match(/^direct child -.*/gm);
-    // let childrenArray = test.match(/^children -.*/gm);
-    // let parsed = [];
+    let parsedData = [];
 
-    let testArray = test.split(">");
+    for (let i = 0; i < dataArray.length; i++) {
+      if (dataArray[i].includes("parent")) {
+        let data = {};
+        data.parent = dataArray[i];
 
-    let filteredArray = testArray.filter(
-      (each) =>
-        each.includes("parent") ||
-        each.includes("direct child") ||
-        each.includes("children")
-    );
-    // .map((each) =>
-    //   each
-    //     .replace(/([\w ]+ - )/g, "")
-    //     .replace(/(\s)*<\/?\w{2}?/g, "")
-    //     .trim()
-    // );
+        for (let j = i + 1; j < dataArray.length; j++) {
+          data.direct_children = [];
 
-    console.log(filteredArray);
+          if (dataArray[j].includes("direct child")) {
+            let childData = { name: dataArray[j] };
+
+            data.direct_children.push(childData);
+            // console.log(data);
+            for (let k = j + 1; k < dataArray.length; k++) {
+              if (dataArray[k].includes("children")) {
+                childData.children = [];
+
+                childData.children.push(dataArray[k]);
+              } else {
+                break;
+              }
+            }
+          } else {
+            break;
+          }
+        }
+
+        parsedData.push(data);
+      }
+    }
+
+    // console.log(dataArray);
+    console.log(parsedData);
   }
+
+  // let ex = {
+  //   parent: "chapter 1",
+  //   direct_children: [
+  //     { name: " title 1" },
+  //     { name: "title 2", children: ["title 2.1", "title 2.2"] },
+  //   ],
+  // };
 
   return (
     <div>
